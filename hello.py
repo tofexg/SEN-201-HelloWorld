6160d8921acdde4337b6aeadac50738374e61599
tofexg <salakotofunmi0@gmail.com>
2026-01-22 20:56:34 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+# SEN 201 - Hello World Program
+# Name: Salako Oluwatofunmi Ayomiposi
+# Matric Number: 24/15146
+
+print("Hello, World!")
+print("Name: Salako Oluwatofunmi Ayomiposi")
+print("Matric Number: 24/15146")
